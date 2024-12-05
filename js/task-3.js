ef7f74386e117acf128f04a2fb9b2270da53d592
Lena <stlena113@gmail.com>
2024-12-05 22:35:35 +0200

    first commit

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function checkForSpam(message) {
+    // Приведення рядка до нижнього регістру для уніфікованої перевірки
+    const lowerCaseMessage = message.toLowerCase();
+
+    // Перевірка наявності заборонених слів
+    if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+        return true;
+    }
+
+    return false;
+}
+
+// Приклад використання функції
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
